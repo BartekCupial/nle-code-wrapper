962e3784b0edf8abb320a750692cbee1170ca78e
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-09-06 10:08:34 +0200

    remove find stairs

--- nle_code_wrapper/strategies/__init__.py
+++ nle_code_wrapper/strategies/__init__.py
@@ -1,11 +1,9 @@
 from nle_code_wrapper.strategies.explore import explore
 from nle_code_wrapper.strategies.fight_monster import fight_all_monsters, fight_closest_monster
-from nle_code_wrapper.strategies.find_stairs import find_stairs
 from nle_code_wrapper.strategies.goto_stairs import goto_stairs
 from nle_code_wrapper.strategies.open_doors import open_doors
 
 __all__ = [
-    find_stairs,
     explore,
     goto_stairs,
     fight_closest_monster,
