6c6c96a8876a563ebdc3c4483f1caf9777cb06be
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-09-10 15:59:00 +0200

    write strategy for keyroom

--- tests/minihack/test_keyroom.py
+++ tests/minihack/test_keyroom.py
@@ -1,19 +1,76 @@
 import pytest
+from nle.nethack import actions as A
+from nle_utils.glyph import G
 
 from nle_code_wrapper.bot.bot import Bot
 from nle_code_wrapper.bot.exceptions import BotPanic
 from nle_code_wrapper.envs.minihack.play_minihack import parse_minihack_args
 from nle_code_wrapper.play import play
 from nle_code_wrapper.plugins.strategy import Strategy
-from nle_code_wrapper.plugins.strategy.strategies import (
-    explore,
-    explore_items,
-    goto_items,
-    goto_stairs,
-    open_doors,
-    open_doors_key,
-    pickup_items,
-)
+from nle_code_wrapper.plugins.strategy.strategies import explore, goto_stairs, open_doors_key
+from nle_code_wrapper.utils import utils
+
+
+def position_of_closest_object(bot, obj):
+    coords = utils.coords(bot.glyphs, obj)
+    distances = bot.pathfinder.distances(bot.entity.position)
+
+    position = min(
+        (e for e in coords if e in distances),
+        key=lambda e: distances[e],
+        default=None,
+    )
+
+    return position
+
+
+def position_of_reachable_adjacent_object(bot: "Bot", obj):
+    coords = utils.coords(bot.glyphs, obj)
+
+    reachable = min(
+        (e for e in coords if bot.pathfinder.reachable_adjacent(bot.entity.position, e)),
+        key=lambda e: bot.pathfinder.distance(bot.entity.position, e),
+        default=None,
+    )
+
+    return reachable
+
+
+@Strategy.wrap
+def pickup_key(bot: "Bot"):
+    position = position_of_closest_object(bot, G.TOOL_CLASS)
+
+    if position:
+        bot.pathfinder.goto(position)
+        bot.step(A.Command.PICKUP)
+        yield True
+    else:
+        yield False
+
+
+@Strategy.wrap
+def general_key(bot: "Bot"):
+    stairs_strat = goto_stairs(bot)
+    key_strat = pickup_key(bot)
+    door_strat = open_doors_key(bot)
+    explore_strat = explore(bot)
+
+    has_key = False
+    while True:
+        try:
+            closed_doors = position_of_reachable_adjacent_object(bot, G.DOOR_CLOSED)
+
+            if stairs_strat():
+                pass
+            elif has_key and closed_doors:
+                door_strat()
+            elif key_strat():
+                has_key = True
+            else:
+                explore_strat()
+        except BotPanic:
+            pass
+        yield True
 
 
 @pytest.mark.usefixtures("register_components")
@@ -24,69 +81,12 @@ class TestMazewalkMapped(object):
             ("keyroom_small_fixed", 7),
             ("keyroom_small", 7),
             ("keyroom_big", 7),
-        ],
-    )
-    def test_keyroom_easy(self, env, seed):
-        cfg = parse_minihack_args(argv=[f"--env={env}", "--no-render", f"--seed={seed}"])
-
-        @Strategy.wrap
-        def general_key(bot: "Bot"):
-            stairs_strat = goto_stairs(bot)
-            door_strat = open_doors(bot)
-            key_strat = open_doors_key(bot)
-            explore_items_strat = explore_items(bot)
-            pickup_items_strat = pickup_items(bot)
-            explore_strat = explore(bot)
-
-            while True:
-                try:
-                    stairs_strat()
-                    explore_items_strat()
-                    pickup_items_strat()
-                    door_strat()
-                    key_strat()
-                    explore_strat()
-                except BotPanic:
-                    pass
-                yield True
-
-        status = play(cfg, strategies=[general_key])
-        assert status == "TASK_SUCCESSFUL"
-
-    @pytest.mark.parametrize(
-        "env, seed",
-        [
             ("keyroom_small_dark", 7),
             ("keyroom_big_dark", 7),
         ],
     )
     def test_keyroom_hard(self, env, seed):
         # TODO: this is quite weak strategy for general exploration of the levels
-
         cfg = parse_minihack_args(argv=[f"--env={env}", "--no-render", f"--seed={seed}"])
-
-        @Strategy.wrap
-        def general_key(bot: "Bot"):
-            stairs_strat = goto_stairs(bot)
-            door_strat = open_doors(bot)
-            key_strat = open_doors_key(bot)
-            goto_items_strat = goto_items(bot)
-            pickup_items_strat = pickup_items(bot)
-            explore_strat = explore(bot)
-
-            while True:
-                try:
-                    if explore_strat():
-                        pass
-                    else:
-                        stairs_strat()
-                        goto_items_strat()
-                        pickup_items_strat()
-                        door_strat()
-                        key_strat()
-                except BotPanic:
-                    pass
-                yield True
-
         status = play(cfg, strategies=[general_key])
         assert status == "TASK_SUCCESSFUL"
