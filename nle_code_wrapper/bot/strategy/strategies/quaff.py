f0ece39268f3b7aa0fab666fbab0ef3abe89e15b
Jens Tuyls <jtuyls@princeton.edu>
2024-10-28 17:04:54 -0400

    add quaff_potion_from_inv strategy

---
+++ nle_code_wrapper/bot/strategy/strategies/quaff.py
@@ -0,0 +1,25 @@
+from nle.nethack import actions as A
+from nle_utils.glyph import G
+
+from nle_code_wrapper.bot.bot import Bot
+from nle_code_wrapper.bot.strategy import State, Strategy
+
+
+@Strategy.wrap
+def quaff_potion_from_inv(bot: "Bot"):
+    inv_glyphs = bot.inv_glyphs
+    inv_letters = bot.inv_letters
+
+    # find the first potion in the inventory
+    potion_char = None
+    for char, glyph in zip(inv_letters, inv_glyphs):
+        if glyph in G.POTION_CLASS:
+            potion_char = char
+            break
+
+    if potion_char is None:
+        yield False
+    else:
+        bot.step(A.Command.QUAFF)
+        bot.step(potion_char)
+        yield True
