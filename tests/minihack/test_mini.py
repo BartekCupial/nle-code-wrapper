bc862988ea70dd14b9bbf01069c16d76762f2eb0
Jens Tuyls <jtuyls@princeton.edu>
2024-10-28 17:08:08 -0400

    add remaining harder envs for simple skills

--- tests/minihack/test_mini.py
+++ tests/minihack/test_mini.py
@@ -106,3 +106,69 @@ class TestMazewalkMapped(object):
         cfg.strategies = [open_doors_kick, explore, goto_stairs]
         status = play(cfg)
         assert status["end_status"].name == "TASK_SUCCESSFUL"
+
+    @pytest.mark.parametrize("env", ["MiniHack-LockedDoor-v0"])
+    @pytest.mark.parametrize("seed", [0])
+    def test_mini_locked_dynamic(self, env, seed):
+        cfg = parse_minihack_args(argv=[f"--env={env}", "--no-render", f"--seed={seed}"])
+        bot = Bot(cfg)
+
+        bot.strategy(open_doors_kick)
+        bot.strategy(explore)
+        bot.strategy(goto_stairs)
+
+        cfg.strategies = [open_doors_kick, explore, goto_stairs]
+        status = play(cfg)
+        assert status["end_status"].name == "TASK_SUCCESSFUL"
+
+    @pytest.mark.parametrize(
+        "env, where, action",
+        [
+            ("MiniHack-Eat-v0", G.FOOD_OBJECTS, partial(make_action_and_confirm, command=A.Command.EAT)),
+            ("MiniHack-Pray-v0", G.ALTAR, partial(make_action_and_confirm, command=A.Command.PRAY)),
+            ("MiniHack-Sink-v0", G.SINK, partial(make_action_and_confirm, command=A.Command.QUAFF)),
+            ("MiniHack-Read-v0", G.SCROLL_CLASS, partial(pickup_and_use_item, command=A.Command.READ)),
+            ("MiniHack-Zap-v0", G.WAND_CLASS, partial(pickup_and_use_item, command=A.Command.ZAP)),
+            (
+                "MiniHack-PutOn-v0",
+                frozenset.union(G.AMULET_CLASS, G.RING_CLASS),
+                partial(pickup_and_use_item, command=A.Command.PUTON),
+            ),
+            ("MiniHack-Wear-v0", G.ARMOR_CLASS, partial(pickup_and_use_item, command=A.Command.WEAR)),
+            ("MiniHack-Wield-v0", G.WEAPON_CLASS, partial(pickup_and_use_item, command=A.Command.WIELD)),
+        ],
+    )
+    @pytest.mark.parametrize("seed", [100])
+    def test_mini_dynamic(self, env, where, action, seed):
+        # TODO: for some of the variants there are monsters which have to be dealt with
+        # TODO: for some of the seeds there are items already worn, which have to be taken off
+        cfg = parse_minihack_args(argv=[f"--env={env}", "--no-render", f"--seed={seed}"])
+        cfg.strategies = [partial(general_mini, where=where, action=action)]
+        status = play(cfg)
+        assert status["end_status"].name == "TASK_SUCCESSFUL"
+
+    @pytest.mark.parametrize(
+        "env, where, action",
+        [
+            ("MiniHack-Eat-Distr-v0", G.FOOD_OBJECTS, partial(make_action_and_confirm, command=A.Command.EAT)),
+            ("MiniHack-Pray-Distr-v0", G.ALTAR, partial(make_action_and_confirm, command=A.Command.PRAY)),
+            ("MiniHack-Sink-Distr-v0", G.SINK, partial(make_action_and_confirm, command=A.Command.QUAFF)),
+            ("MiniHack-Read-Distr-v0", G.SCROLL_CLASS, partial(pickup_and_use_item, command=A.Command.READ)),
+            ("MiniHack-Zap-Distr-v0", G.WAND_CLASS, partial(pickup_and_use_item, command=A.Command.ZAP)),
+            (
+                "MiniHack-PutOn-Distr-v0",
+                frozenset.union(G.AMULET_CLASS, G.RING_CLASS),
+                partial(pickup_and_use_item, command=A.Command.PUTON),
+            ),
+            ("MiniHack-Wear-Distr-v0", G.ARMOR_CLASS, partial(pickup_and_use_item, command=A.Command.WEAR)),
+            ("MiniHack-Wield-Distr-v0", G.WEAPON_CLASS, partial(pickup_and_use_item, command=A.Command.WIELD)),
+        ],
+    )
+    @pytest.mark.parametrize("seed", [100])
+    def test_mini_distract(self, env, where, action, seed):
+        # TODO: for some of the variants there are monsters which have to be dealt with
+        # TODO: for some of the seeds there are items already worn, which have to be taken off
+        cfg = parse_minihack_args(argv=[f"--env={env}", "--no-render", f"--seed={seed}"])
+        cfg.strategies = [partial(general_mini, where=where, action=action)]
+        status = play(cfg)
+        assert status["end_status"].name == "TASK_SUCCESSFUL"
