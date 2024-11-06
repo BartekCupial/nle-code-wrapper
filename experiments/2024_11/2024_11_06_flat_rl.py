706933344bdf2c05479f927feeee377848ff6774
Bartek Cupiał <bartlomiej.cupial@gmail.com>
2024-11-06 13:05:58 +0100

    flat rl experiments for JT model

---
+++ experiments/2024_11/2024_11_06_flat_rl.py
@@ -0,0 +1,60 @@
+import os
+
+from mrunner.helpers.specification_helper import create_experiments_helper
+
+name = globals()["script"][:-3]
+
+# params for all exps
+config = {
+    "exp_tags": [name],
+    "train_for_env_steps": 1_000_000,
+    "num_workers": 16,
+    "num_envs_per_worker": 32,
+    "worker_num_splits": 2,
+    "rollout": 32,
+    "batch_size": 4096,  # this equals bs = 128, 128 * 32 = 4096
+    "penalty_time": -0.001,
+    "async_rl": True,
+    "serial_mode": False,
+    "wandb_user": "bartekcupial",
+    "wandb_project": "nle_code_wrapper",
+    "wandb_group": "ideas-ncbr",
+    "with_wandb": True,
+    "decorrelate_envs_on_one_worker": False,
+    "code-wrapper": False,
+}
+
+# params different between exps
+params_grid = [
+    {
+        "seed": list(range(5)),
+        "gamma": [0.99],
+        "restart_behavior": ["overwrite"],
+        "env": [env],
+        "exp_point": [env],
+        "group": [env],
+    }
+    for env in [
+        "MiniHack-Corridor-R2-v0",
+        "MiniHack-Corridor-R3-v0",
+        "MiniHack-Corridor-R5-v0",
+        "MiniHack-CorridorBattle-v0",
+        "MiniHack-CorridorBattle-Dark-v0",
+        "MiniHack-KeyRoom-Dark-S5-v0",
+    ]
+]
+
+experiments_list = create_experiments_helper(
+    experiment_name=name,
+    project_name="nle_code_wrapper",
+    with_neptune=False,
+    script="python3 mrunner_run.py",
+    python_path=".",
+    tags=[name],
+    env={
+        "WANDB_API_KEY": os.environ["WANDB_API_KEY"],
+    },
+    base_config=config,
+    params_grid=params_grid,
+    mrunner_ignore=".mrunnerignore",
+)
