6adb23a7450c800afcda628563c6a9d0edc15fc5
Bartek Cupiał <bartlomiej.cupial@gmail.com>
2024-11-06 13:15:44 +0100

    add chaotic dwarf

--- nle_code_wrapper/agents/sample_factory/minihack/models/__init__.py
+++ nle_code_wrapper/agents/sample_factory/minihack/models/__init__.py
@@ -1,6 +1,8 @@
+from nle_code_wrapper.agents.sample_factory.minihack.models.chaotic_dwarf import ChaoticDwarvenGPT5
 from nle_code_wrapper.agents.sample_factory.minihack.models.scaled import ScaledNet
 
 MODELS = [
     ScaledNet,
+    ChaoticDwarvenGPT5,
 ]
 MODELS_LOOKUP = {c.__name__: c for c in MODELS}
