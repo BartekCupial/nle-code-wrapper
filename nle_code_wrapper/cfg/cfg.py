b3fc9120447a27ab9c57e2d93052f3c82f6f4914
Bartek Cupiał <bartlomiej.cupial@gmail.com>
2024-11-07 16:32:15 +0100

    turn off jit for Chaotic dwarven

--- nle_code_wrapper/cfg/cfg.py
+++ nle_code_wrapper/cfg/cfg.py
@@ -5,7 +5,7 @@ from nle_utils.utils.utils import str2bool
 
 
 def add_code_wrapper_cli_args(p: ArgumentParser):
-    p.add_argument("--code-wrapper", type=str2bool, default=True, help="Do we want to wrap the env with code wrapper")
+    p.add_argument("--code_wrapper", type=str2bool, default=True, help="Do we want to wrap the env with code wrapper")
     p.add_argument("--strategies", type=ast.literal_eval, default=[], help="List of strategy names")
     p.add_argument(
         "--strategies_loc",
