01a7da931cbd3f4b3e38f087b225e2b8525b88b1
Bartek Cupiał <bartlomiej.cupial@gmail.com>
2024-11-07 15:42:37 +0100

    add strategy steps to observation space so we can do discounting

--- nle_code_wrapper/wrappers/nle_code_wrapper.py
+++ nle_code_wrapper/wrappers/nle_code_wrapper.py
@@ -1,6 +1,7 @@
 from typing import Callable, List
 
 import gymnasium as gym
+import numpy as np
 
 from nle_code_wrapper.bot import Bot
 
@@ -13,13 +14,18 @@ class NLECodeWrapper(gym.Wrapper):
         for strategy_func in strategies:
             self.bot.strategy(strategy_func)
         self.action_space = gym.spaces.Discrete(len(self.bot.strategies))
+        self.observation_space = gym.spaces.Dict(
+            {"strategy_steps": gym.spaces.Box(low=0, high=255, shape=(1,)), **self.env.observation_space}
+        )
 
     def reset(self, **kwargs):
         obs, info = self.bot.reset(**kwargs)
+        obs["strategy_steps"] = np.array([info["episode_extra_stats"]["strategy_steps"]])
 
         return obs, info
 
     def step(self, action):
         obs, reward, terminated, truncated, info = self.bot.strategy_step(action)
+        obs["strategy_steps"] = np.array([info["episode_extra_stats"]["strategy_steps"]])
 
         return obs, reward, terminated, truncated, info
