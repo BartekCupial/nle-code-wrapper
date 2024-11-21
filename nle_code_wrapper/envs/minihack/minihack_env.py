8e714845930a1a2fb17310d3062ab4a43d9faa26
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-11-21 10:51:01 +0100

    allow no strategies when code_wrapper is false

--- nle_code_wrapper/envs/minihack/minihack_env.py
+++ nle_code_wrapper/envs/minihack/minihack_env.py
@@ -9,7 +9,7 @@ from nle_code_wrapper.wrappers.nle_code_wrapper import NLECodeWrapper
 def make_minihack_env(env_name, cfg, env_config, render_mode: Optional[str] = None):
     env = make_env(env_name=env_name, cfg=cfg, env_config=env_config, render_mode=render_mode)
 
-    if isinstance(cfg.strategies[0], str):
+    if len(cfg.strategies) > 0 and isinstance(cfg.strategies[0], str):
         strategies = []
         for strategy_name in cfg.strategies:
             strategy_func = get_function_by_name(cfg.strategies_loc, strategy_name)
