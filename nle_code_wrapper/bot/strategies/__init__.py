caaca1a37dbaaa228041a5c43b267801b75a1c42
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-11-21 15:56:23 +0100

    initial exploration strategies

--- nle_code_wrapper/bot/strategies/__init__.py
+++ nle_code_wrapper/bot/strategies/__init__.py
@@ -1,3 +1,12 @@
+from nle_code_wrapper.bot.strategies.exploration_strategies import (
+    explore_corridor,
+    explore_room,
+    explore_room_systematically,
+    goto_next_unexplored_room,
+    search_corridor_for_hidden_doors,
+    search_for_traps,
+    search_room_for_hidden_doors,
+)
 from nle_code_wrapper.bot.strategies.explore import explore
 from nle_code_wrapper.bot.strategies.explore_items import explore_items
 from nle_code_wrapper.bot.strategies.fight_monster import fight_all_monsters, fight_closest_monster
@@ -27,4 +36,11 @@ __all__ = [
     goto_items,
     quaff_potion_from_inv,
     goto,
+    search_for_traps,
+    explore_room_systematically,
+    explore_room,
+    goto_next_unexplored_room,
+    explore_corridor,
+    search_corridor_for_hidden_doors,
+    search_room_for_hidden_doors,
 ]
