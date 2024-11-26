0654ede42a4cf80be9b8839f8574665c1a28c9c3
Jens Tuyls <jtuyls@princeton.edu>
2024-11-26 14:29:11 -0500

    skill wrapper cli

--- nle_code_wrapper/cfg/cfg.py
+++ nle_code_wrapper/cfg/cfg.py
@@ -13,3 +13,20 @@ def add_code_wrapper_cli_args(p: ArgumentParser):
         default="nle_code_wrapper.bot.strategies",
         help="where to search for strategies implementation",
     )
+
+
+def add_skill_wrapper_cli_args(p: ArgumentParser):
+    p.add_argument("--skill-wrapper", type=str2bool, default=True, help="Do we want to wrap the env with skill wrapper")
+    p.add_argument("--skill_dim", type=int, default=2, help="Dimension of the skill space")
+    p.add_argument("--num_negative_z", type=int, default=256, help="Number of negative samples for the skill space")
+    p.add_argument("--csf_lam", type=float, default=1.0, help="CSF lambda")
+    p.add_argument("--duplicate_skills", type=int, default=1, help="Number of duplicate skills")
+    p.add_argument("--dual_lam", type=float, default=30.0, help="Dual lambda")
+    p.add_argument(
+        "--skill_method", type=str, default="CSF", help="Method to use for skill learning.", choices=["CSF", "METRA"]
+    )
+    p.add_argument("--dual_slack", type=float, default=1e-3)
+    p.add_argument(
+        "--skill_type", type=str, default="continuous", help="Type of skill space", choices=["continuous", "discrete"]
+    )
+    p.add_argument("--te_learning_rate", type=float, default=1e-4)
