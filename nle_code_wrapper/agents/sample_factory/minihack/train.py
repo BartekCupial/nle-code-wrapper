a335c3fbdaa1ff7c2822a791d55c58fd807c68ec
Jens Tuyls <jtuyls@princeton.edu>
2024-11-26 14:43:50 -0500

    add skill cli, remove tmp files at startup to prevent device error

--- nle_code_wrapper/agents/sample_factory/minihack/train.py
+++ nle_code_wrapper/agents/sample_factory/minihack/train.py
@@ -16,7 +16,7 @@ from nle_code_wrapper.agents.sample_factory.minihack.minihack_params import (
     minihack_override_defaults,
 )
 from nle_code_wrapper.agents.sample_factory.minihack.models import MODELS_LOOKUP
-from nle_code_wrapper.cfg.cfg import add_code_wrapper_cli_args
+from nle_code_wrapper.cfg.cfg import add_code_wrapper_cli_args, add_skill_wrapper_cli_args
 
 
 def register_minihack_envs():
@@ -24,14 +24,14 @@ def register_minihack_envs():
         register_env(env_name, make_minihack_env)
 
 
-def make_minihack_encoder(cfg: Config, obs_space: ObsSpace) -> Encoder:
+def make_minihack_encoder(cfg: Config, obs_space: ObsSpace, ignore_option: bool = False) -> Encoder:
     """Factory function as required by the API."""
     try:
         model_cls = MODELS_LOOKUP[cfg.model]
     except KeyError:
         raise NotImplementedError("model=%s" % cfg.model) from None
 
-    return model_cls(cfg, obs_space)
+    return model_cls(cfg, obs_space, ignore_option=ignore_option)
 
 
 def register_minihack_components():
@@ -45,14 +45,39 @@ def parse_minihack_args(argv=None, evaluation=False):
     add_extra_params_model(parser)
     add_extra_params_general(parser)
     add_code_wrapper_cli_args(parser)
+    add_skill_wrapper_cli_args(parser)
     minihack_override_defaults(partial_cfg.env, parser)
     final_cfg = parse_full_cfg(parser, argv)
 
     return final_cfg
 
 
+def delete_nle_folders_in_tmp():
+    import os
+    import shutil
+
+    tmp_dir = "/tmp"
+    prefix = "nle"
+
+    try:
+        # List all directory entries in the /tmp directory
+        for entry in os.listdir(tmp_dir):
+            full_path = os.path.join(tmp_dir, entry)
+
+            # Check if the entry is a directory and starts with the desired prefix
+            if os.path.isdir(full_path) and entry.startswith(prefix):
+                # Delete the directory
+                shutil.rmtree(full_path)
+                print(f"Deleted: {full_path}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 def main():  # pragma: no cover
     """Script entry point."""
+    # delete_nle_folders_in_tmp()
+
     register_minihack_components()
     cfg = parse_minihack_args()
     status = run_rl(cfg)
