5753b525721f5df217b1fb300dbd0f9eb7aae72d
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-12-01 08:19:38 +0100

    experiments on quest

---
+++ experiments/2024_11/2024_11_29_hierarchical_rl_quest2.py
@@ -0,0 +1,73 @@
+import os
+
+from mrunner.helpers.specification_helper import create_experiments_helper
+
+name = globals()["script"][:-3]
+
+# params for all exps
+config = {
+    "exp_tags": [name],
+    "train_for_env_steps": 20_000_000,
+    "num_workers": 16,
+    "num_envs_per_worker": 32,
+    "worker_num_splits": 2,
+    "rollout": 32,
+    "batch_size": 4096,  # this equals bs = 128, 128 * 32 = 4096
+    "penalty_step": 0.0,
+    "penalty_time": 0.0,
+    "async_rl": True,
+    "serial_mode": False,
+    "wandb_user": "bartekcupial",
+    "wandb_project": "nle_code_wrapper",
+    "wandb_group": "ideas-ncbr",
+    "with_wandb": True,
+    "decorrelate_envs_on_one_worker": False,
+    "code_wrapper": True,
+    "hierarchical_gamma": True,  # should be the same as code_wrapper
+}
+
+strategies = [
+    "explore_corridor",
+    "explore_room",
+    "fight_closest_monster",
+    "goto_closest_corridor",
+    "goto_closest_room",
+    "zap",
+]
+
+
+# params different between exps
+params_grid = [
+    {
+        "seed": list(range(3)),
+        "model": ["ChaoticDwarvenGPT5"],
+        "gamma": [0.999],
+        "exploration_loss_coeff": [0.001, 0.003, 0.01],
+        "strategies": [strategies],
+        "restart_behavior": ["overwrite"],
+        "env": [env],
+        "exp_point": [env],
+        "group": [env],
+    }
+    for env in [
+        "MiniHack-Quest-Easy-v0",
+        "MiniHack-Quest-Medium-v0",
+        "MiniHack-Quest-Hard-v0",
+    ]
+]
+
+
+experiments_list = create_experiments_helper(
+    experiment_name=name,
+    project_name="nle_code_wrapper",
+    with_neptune=False,
+    script="python3 mrunner_run.py",
+    python_path=".",
+    tags=[name],
+    env={
+        "WANDB_API_KEY": os.environ["WANDB_API_KEY"],
+    },
+    base_config=config,
+    params_grid=params_grid,
+    mrunner_ignore=".mrunnerignore",
+)
