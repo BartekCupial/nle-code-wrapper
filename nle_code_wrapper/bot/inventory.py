ca910ade511fa59d7714efe739b73425a4295d33
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-12-05 11:47:17 +0100

    parse object for items

--- nle_code_wrapper/bot/inventory.py
+++ nle_code_wrapper/bot/inventory.py
@@ -1,7 +1,38 @@
 from typing import Dict, List
 
 import numpy as np
+from nle import nethack
 from nle_utils.item import ItemBeatitude, ItemClasses, ItemEnchantment, ItemShopStatus
+
+OBJECT_NAMES = {
+    (nethack.OBJ_NAME(nethack.objclass(index)), nethack.objclass(index).oc_class): nethack.objclass(index)
+    for index in range(nethack.NUM_OBJECTS)
+}
+OBJECT_DESCRS = {
+    (nethack.OBJ_DESCR(nethack.objclass(index)), nethack.objclass(index).oc_class): nethack.objclass(index)
+    for index in range(nethack.NUM_OBJECTS)
+}
+
+
+def get_object(full_name, obj_class):
+    candidates = []
+    for (obj_name, oc_class), obj in OBJECT_NAMES.items():
+        if obj_name is None:
+            continue
+
+        if obj_name in full_name and chr(obj_class.value) == oc_class:
+            candidates.append((obj_name, obj))
+
+    if len(candidates) == 0:
+        for (obj_descr, oc_class), obj in OBJECT_DESCRS.items():
+            if obj_descr is None:
+                continue
+
+            if obj_descr in full_name and chr(obj_class.value) == oc_class:
+                candidates.append((obj_descr, obj))
+
+    assert len(candidates) == 1, f"Multiple candidates found: {candidates}"
+    return candidates[0][1]
 
 
 class Item:
@@ -13,54 +44,71 @@ class Item:
         glyph: int,
         shop_status: int = None,
         value: int = None,
-        weight: int = None,
     ):
         self.letter = letter
-        self.name = "".join(map(chr, name)).rstrip("\x00")
+        self.full_name = "".join(map(chr, name)).rstrip("\x00")
         self.item_class = ItemClasses(oclass)
         self.glyph = glyph
 
         self.shop_status = ItemShopStatus(shop_status) if shop_status is not None else None
         self.value = value
-        self.weight = weight
+
+    @property
+    def object(self):
+        return get_object(self.full_name, self.item_class)
+
+    @property
+    def weight(self):
+        """
+        Calculate the weight of the given object, including recursive calculation
+        of contained objects' weights.
+
+        Args:
+            obj: The object to calculate weight for
+
+        Returns:
+            int: Total weight of the object
+        """
+        # TODO: recursive calculation
+        return self.object.oc_weight
 
     @property
     def beatitude(self):
-        if "blessed" in self.name:
+        if "blessed" in self.full_name:
             return ItemBeatitude.BLESSED
-        elif "uncursed" in self.name:
+        elif "uncursed" in self.full_name:
             return ItemBeatitude.UNCURSED
-        elif "cursed" in self.name:
+        elif "cursed" in self.full_name:
             return ItemBeatitude.CURSED
         else:
             return ItemBeatitude.UNKNOWN
 
     @property
     def in_use(self):
-        if "(" in self.name:
+        if "(" in self.full_name:
             return True
         else:
             return False
 
     @property
     def quantity(self):
-        if self.name.split(" ")[0].isdigit():
-            return int(self.name.split(" ")[0])
+        if self.full_name.split(" ")[0].isdigit():
+            return int(self.full_name.split(" ")[0])
         else:
             return 1
 
     @property
     def enchantment(self):
-        if "+" in self.name:
-            ench = int(self.name.split("+")[1].split(" ")[0])
-        elif "-" in self.name:
-            ench = int(self.name.split("-")[1].split(" ")[0])
+        if "+" in self.full_name:
+            ench = int(self.full_name.split("+")[1].split(" ")[0])
+        elif "-" in self.full_name:
+            ench = int(self.full_name.split("-")[1].split(" ")[0])
         else:
             ench = None
         return ItemEnchantment(ench)
 
     def __str__(self):
-        return f"{chr(self.letter)}) {self.name}"
+        return f"{chr(self.letter)}) {self.full_name}"
 
 
 class Inventory:
@@ -132,4 +180,11 @@ if __name__ == "__main__":
     inv_strs = obs["inv_strs"]
 
     inventory = Inventory(inv_strs, inv_letters, inv_oclasses, inv_glyphs)
+    for item in inventory.items:
+        item.object
+        item.weight
+        item.beatitude
+        item.in_use
+        item.quantity
+        item.enchantment
     print(inventory)
