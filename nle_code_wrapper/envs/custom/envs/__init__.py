b55e086d4d2c844ac7a1bf916735fdff0076dc0e
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-12-10 10:26:21 +0100

    update cross lava river

--- nle_code_wrapper/envs/custom/envs/__init__.py
+++ nle_code_wrapper/envs/custom/envs/__init__.py
@@ -1,1 +1,1 @@
-from nle_code_wrapper.envs.custom.envs import key_room_items, premapped_corridor, skills_simple
+from nle_code_wrapper.envs.custom.envs import key_room_items, premapped_corridor, skills_quest, skills_simple
