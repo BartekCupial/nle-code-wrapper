7187f81c194adf55f84abef9a436b9fe21c4526d
Jens Tuyls <jtuyls@princeton.edu>
2025-01-06 15:05:55 -0500

    remove tokenization wrapper

--- nle_code_wrapper/agents/sample_factory/minihack/minihack_env.py
+++ nle_code_wrapper/agents/sample_factory/minihack/minihack_env.py
@@ -5,7 +5,6 @@ from nle_utils.envs.minihack.minihack_env import make_minihack_env as make_env
 from nle_code_wrapper.agents.sample_factory.minihack.wrappers import AddChanngelDim
 from nle_code_wrapper.utils.utils import get_function_by_name
 from nle_code_wrapper.wrappers.nle_code_wrapper import NLECodeWrapper
-from nle_code_wrapper.wrappers.tokenization_wrapper import TokenizationWrapper
 
 
 def make_minihack_env(env_name, cfg, env_config, render_mode: Optional[str] = None):
@@ -26,10 +25,5 @@ def make_minihack_env(env_name, cfg, env_config, render_mode: Optional[str] = No
 
     if cfg.model == "default_make_encoder_func":
         env = AddChanngelDim(env)
-
-    if cfg.use_lm:
-        env = TokenizationWrapper(
-            env, cfg.lm_model_name, strategies=cfg.strategies, lm_max_obs_tokens=cfg.lm_max_obs_tokens
-        )
 
     return env
