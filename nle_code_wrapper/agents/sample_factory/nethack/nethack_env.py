a8181e9380b8650bdd1a5ec1ef9b59e32fcb91dc
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-01-07 07:34:16 +0100

    nethack score works

--- nle_code_wrapper/agents/sample_factory/nethack/nethack_env.py
+++ nle_code_wrapper/agents/sample_factory/nethack/nethack_env.py
@@ -2,6 +2,7 @@ from typing import Optional, Tuple
 
 import gym
 import nle  # NOQA: F401
+from nle import nethack
 from nle.nethack import NETHACKOPTIONS
 from nle_utils.wrappers import GymV21CompatibilityV0, NLETimeLimit
 
@@ -43,6 +44,10 @@ def make_nethack_env(env_name, cfg, env_config, render_mode: Optional[str] = Non
         penalty_mode=cfg.fn_penalty_step,
         savedir=cfg.savedir,
         save_ttyrec_every=cfg.save_ttyrec_every,
+        wizard=False,
+        allow_all_yn_questions=True,
+        allow_all_modes=True,
+        actions=nethack.ACTIONS,
     )
 
     if cfg.max_episode_steps is not None:
