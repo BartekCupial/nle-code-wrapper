d4bf48244c2fb4f5a9964d4974849ceb39fcb5c8
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-01-18 19:57:40 +0100

    add oracle test

---
+++ nle_code_wrapper/envs/minihack/envs/oracle.py
@@ -0,0 +1,16 @@
+from minihack.envs import register
+from minihack.envs.keyroom import MiniHackKeyDoor
+
+
+class CustomMiniHackOracle(MiniHackKeyDoor):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_episode_steps"] = kwargs.pop("max_episode_steps", 100)
+        kwargs["allow_all_yn_questions"] = kwargs.pop("allow_all_yn_questions", True)
+        kwargs["allow_all_modes"] = kwargs.pop("allow_all_modes", True)
+        super().__init__(*args, des_file="nle_code_wrapper/envs/minihack/dat/oracle.des", **kwargs)
+
+
+register(
+    id="CustomMiniHack-Oracle-v0",
+    entry_point="nle_code_wrapper.envs.minihack.envs.oracle:CustomMiniHackOracle",
+)
