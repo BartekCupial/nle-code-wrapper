f8c7f3c12b251999d8e42ef4e95a6a9b678d0358
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-01-20 21:33:36 +0100

    configs for playing the games with sets of strategies

---
+++ experiments/2025_01/play/2025_01_20_hideNseek.py
@@ -0,0 +1,77 @@
+import os
+
+from mrunner.helpers.specification_helper import create_experiments_helper
+
+from nle_code_wrapper.utils.granularity import easy, item, navigation
+
+name = globals()["script"][:-3]
+
+num_minibatches = 1
+num_epochs = 1
+num_envs = 128
+num_steps = 32
+num_workers = 16
+
+# params for all exps
+config = {
+    "run_script": "nle_code_wrapper.envs.minihack.play_minihack",
+    "code_wrapper": True,
+}
+
+strategies = [
+    "goto_corridor",
+    "goto_corridor_east",
+    "goto_corridor_north",
+    "goto_corridor_south",
+    "goto_corridor_west",
+    "goto_room",
+    "goto_room_east",
+    "goto_room_north",
+    "goto_room_south",
+    "goto_room_west",
+    "goto_downstairs",
+    "goto_upstairs",
+    "explore_corridor",
+    "explore_corridor_east",
+    "explore_corridor_north",
+    "explore_corridor_south",
+    "explore_corridor_west",
+    "explore_room",
+    "explore_room_east",
+    "explore_room_north",
+    "explore_room_south",
+    "explore_room_west",
+    "wait_for_monster",
+    "run_away",
+]
+
+
+# params different between exps
+params_grid = [
+    {
+        "strategies": [strategies],
+        "env": [env],
+    }
+    for env in [
+        "MiniHack-HideNSeek-Mapped-v0",
+        "MiniHack-HideNSeek-v0",
+        "MiniHack-HideNSeek-Lava-v0",
+        "MiniHack-HideNSeek-Big-v0",
+    ]
+]
+
+
+experiments_list = create_experiments_helper(
+    experiment_name=name,
+    project_name="nle_code_wrapper",
+    with_neptune=False,
+    script="python3 mrunner_run.py",
+    python_path=".",
+    tags=[name],
+    env={
+        "WANDB_API_KEY": os.environ["WANDB_API_KEY"],
+    },
+    base_config=config,
+    params_grid=params_grid,
+    mrunner_ignore=".mrunnerignore",
+)
