b850a8732c80a72aeec607bda78fd20c065c3f83
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-01-30 19:17:49 +0100

    update rnd

--- examples/example_use_strategies.py
+++ examples/example_use_strategies.py
@@ -1,5 +1,5 @@
 from nle_code_wrapper.bot.bot import Bot
-from nle_code_wrapper.bot.strategies import descend_stairs, explore, fight_monster
+from nle_code_wrapper.bot.strategies import descend_stairs, explore, fight_melee
 from nle_code_wrapper.envs.minihack.play_minihack import parse_minihack_args, register_minihack_components
 
 
@@ -7,7 +7,7 @@ def main():
     register_minihack_components()
     cfg = parse_minihack_args(argv=["--env=big_room_dark"])
     bot = Bot(cfg)
-    bot.strategy(fight_monster)
+    bot.strategy(fight_melee)
     bot.strategy(descend_stairs)
     bot.strategy(explore)
     bot.main()
--- nle_code_wrapper/agents/sample_factory/minihack/models/rnd_model.py
+++ nle_code_wrapper/agents/sample_factory/minihack/models/rnd_model.py
@@ -1,76 +1,105 @@
+from typing import Dict
+
+import numpy as np
 import torch
-from nle import nethack
 from sample_factory.algo.utils.torch_utils import calc_num_elements
 from sample_factory.model.encoder import Encoder
+from sample_factory.utils.normalize import EPS, RunningMeanStdDictInPlace, copy_dict_structure, iter_dicts_recursively
 from sample_factory.utils.typing import Config, ObsSpace
 from torch import nn
-from torch.nn import functional as F
 
-from nle_code_wrapper.agents.sample_factory.minihack.models.chaotic_dwarf import (
-    BottomLinesEncoder,
-    CharColorEncoder,
-    TopLineEncoder,
-)
+
+class ObservationNormalizer(nn.Module):
+    def __init__(self, obs_space, cfg):
+        super().__init__()
+
+        self.sub_mean = cfg.obs_subtract_mean
+        self.scale = cfg.obs_scale
+
+        self.running_mean_std = RunningMeanStdDictInPlace(obs_space, ["screen_image"])
+
+        self.should_sub_mean = abs(self.sub_mean) > EPS
+        self.should_scale = abs(self.scale - 1.0) > EPS
+        self.should_normalize = self.should_sub_mean or self.should_scale or self.running_mean_std is not None
+
+    @staticmethod
+    def _clone_tensordict(obs_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        obs_clone = copy_dict_structure(obs_dict)  # creates an identical dict but with None values
+        for d, d_clone, k, x, _ in iter_dicts_recursively(obs_dict, obs_clone):
+            if x.dtype != torch.float:
+                # type conversion requires a copy, do this check to make sure we don't do it twice
+                d_clone[k] = x.float()  # this will create a copy of a tensor
+            else:
+                d_clone[k] = x.clone()  # otherwise, we explicitly clone it since normalization is in-place
+
+        return obs_clone
+
+    def forward(self, obs_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        if not self.should_normalize:
+            return obs_dict
+
+        with torch.no_grad():
+            # since we are creating a clone, it is safe to use in-place operations
+            obs_clone = self._clone_tensordict(obs_dict)
+
+            # subtraction of mean and scaling is only applied to default "obs"
+            # this should be modified for custom obs dicts
+            if self.should_sub_mean:
+                obs_clone["obs"].sub_(self.sub_mean)
+
+            if self.should_scale:
+                obs_clone["obs"].mul_(1.0 / self.scale)
+
+            if self.running_mean_std:
+                self.running_mean_std(obs_clone)  # in-place normalization
+
+        return obs_clone
+
+
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    return layer
 
 
 class RNDModel(Encoder):
     def __init__(self, cfg: Config, obs_space: ObsSpace):
         super().__init__(cfg)
-        self.obs_keys = list(sorted(obs_space.keys()))  # always the same order
-        self.encoders = nn.ModuleDict()
 
-        self.use_tty_only = cfg.use_tty_only
-        self.use_prev_action = cfg.use_prev_action
+        self.obs_normalizer: ObservationNormalizer = ObservationNormalizer(obs_space, cfg)
 
-        # screen encoders
-        screen_shape = obs_space["tty_chars"].shape
-        self.predictor_screen_encoder = CharColorEncoder(
-            (screen_shape[0] - 3, screen_shape[1]),
-            char_edim=cfg.char_edim,
-            color_edim=cfg.color_edim,
+        # Predictor Network
+        self.predictor_chars_net = nn.Sequential(
+            layer_init(nn.Conv2d(2, 32, 3, stride=1)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(32, 64, 3, stride=1)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
+            nn.ReLU(),
+            nn.Flatten(),
         )
-        self.target_screen_encoder = CharColorEncoder(
-            (screen_shape[0] - 3, screen_shape[1]),
-            char_edim=cfg.char_edim,
-            color_edim=cfg.color_edim,
-        )
-
-        # top and bottom encoders
-        self.predictor_topline_encoder = TopLineEncoder()
-        self.predictor_bottomline_encoder = torch.jit.script(BottomLinesEncoder())
-        self.target_topline_encoder = TopLineEncoder()
-        self.target_bottomline_encoder = torch.jit.script(BottomLinesEncoder())
-
-        if self.use_prev_action:
-            self.num_actions = obs_space["prev_actions"].n
-            self.prev_actions_dim = self.num_actions
-        else:
-            self.num_actions = None
-            self.prev_actions_dim = 0
-
-        self.encoder_out_size = sum(
-            [
-                self.predictor_topline_encoder.hidden_dim,
-                self.predictor_bottomline_encoder.hidden_dim,
-                self.predictor_screen_encoder.hidden_dim,
-                self.prev_actions_dim,
-            ]
-        )
-
-        self.predictor_proj = nn.Linear(self.encoder_out_size, 256)
-        self.target_proj = nn.Linear(self.encoder_out_size, 256)
-
-        # MAIN
+        self.predictor_proj = nn.Linear(9216, 256)
         self.predictor = nn.Sequential(
-            nn.Linear(256, 512),
+            layer_init(nn.Linear(256, 512)),
             nn.ReLU(),
-            nn.Linear(512, 512),
+            layer_init(nn.Linear(512, 512)),
             nn.ReLU(),
-            nn.Linear(512, 512),
+            layer_init(nn.Linear(512, 512)),
         )
 
+        # Target Network
+        self.target_chars_net = nn.Sequential(
+            layer_init(nn.Conv2d(2, 32, 3, stride=1)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(32, 64, 3, stride=1)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+        self.target_proj = nn.Linear(9216, 256)
         self.target = nn.Sequential(
-            nn.Linear(256, 512),
+            layer_init(nn.Linear(256, 512)),
         )
 
         # target network is not trainable
@@ -78,54 +107,19 @@ class RNDModel(Encoder):
             if name.startswith("target"):
                 param.requires_grad = False
 
-    def encode_hidden(self, obs_dict, bottomline_encoder, topline_encoder, screen_encoder, proj):
-        B, H, W = obs_dict["tty_chars"].shape
-        # to process images with CNNs we need channels dim
-        C = 1
-
-        # Take last channel for now
-        topline = obs_dict["tty_chars"][:, 0].contiguous()
-        bottom_line = obs_dict["tty_chars"][:, -2:].contiguous()
-
-        # Blstats
-        blstats_rep = bottomline_encoder(bottom_line.float(memory_format=torch.contiguous_format).view(B, -1))
-
-        encodings = [
-            topline_encoder(topline.float(memory_format=torch.contiguous_format).view(B, -1)),
-            blstats_rep,
-        ]
-
-        # Main obs encoding
-        tty_chars = (
-            obs_dict["tty_chars"][:, 1:-2]
-            .contiguous()
-            .float(memory_format=torch.contiguous_format)
-            .view(B, C, H - 3, W)
-        )
-        tty_colors = obs_dict["tty_colors"][:, 1:-2].contiguous().view(B, C, H - 3, W)
-        encodings.append(screen_encoder(tty_chars, tty_colors))
-
-        if self.use_prev_action:
-            prev_actions = obs_dict["prev_actions"].long().view(B)
-            encodings.append(torch.nn.functional.one_hot(prev_actions, self.num_actions))
-
-        concat = torch.cat(encodings, dim=1)
-
-        return proj(concat)
+    def encode_hidden(self, obs_dict, chars_net, proj):
+        chars = chars_net(obs_dict["screen_image"])
+        return proj(chars)
 
     def forward(self, obs_dict):
         predict_hidden = self.encode_hidden(
             obs_dict,
-            self.predictor_bottomline_encoder,
-            self.predictor_topline_encoder,
-            self.predictor_screen_encoder,
+            self.predictor_chars_net,
             self.predictor_proj,
         )
         target_hidden = self.encode_hidden(
             obs_dict,
-            self.target_bottomline_encoder,
-            self.target_topline_encoder,
-            self.target_screen_encoder,
+            self.target_chars_net,
             self.target_proj,
         )
 
@@ -133,3 +127,11 @@ class RNDModel(Encoder):
         target_feature = self.target(target_hidden)
 
         return predict_feature, target_feature
+
+
+if __name__ == "__main__":
+    import gym
+    import nle
+
+    env = gym.make("NetHackScore-v0")
+    obs = env.reset()
