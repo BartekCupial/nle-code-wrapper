85e780dc285217e271c00aa565089b46d99c893a
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-02-03 08:08:39 +0100

    move character from nle_utils

---
+++ nle_code_wrapper/bot/character/__init__.py
@@ -0,0 +1,5 @@
+from nle_code_wrapper.bot.character.character import Character
+from nle_code_wrapper.bot.character.properties import Alignment, Gender, Race, Role
+from nle_code_wrapper.bot.character.skill import CharacterSkills, Skill
+
+__all__ = [Character, Skill, CharacterSkills, Role, Race, Gender, Alignment]
