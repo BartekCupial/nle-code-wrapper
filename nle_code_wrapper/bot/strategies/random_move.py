09022519a3a1735d0dd6d18b347677f738bc9d73
Jens Tuyls <jtuyls@princeton.edu>
2025-08-19 13:11:09 -0400

    bug fix: return false if no neighbors in random_move

--- nle_code_wrapper/bot/strategies/random_move.py
+++ nle_code_wrapper/bot/strategies/random_move.py
@@ -10,6 +10,10 @@ def random_move(bot: "Bot") -> bool:
     Randomly move to a neighbor position.
     """
     neighbors = bot.pathfinder.neighbors(bot.entity.position)
+
+    if len(neighbors) == 0:
+        return False
+
     goal = neighbors[np.random.choice(len(neighbors))]
     bot.pathfinder.goto(goal)
     return True
