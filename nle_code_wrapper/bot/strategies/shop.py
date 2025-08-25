8aca8277da285258152ae9eb31342831b297356b
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2025-08-25 10:37:22 +0200

    fix leave shop

--- nle_code_wrapper/bot/strategies/shop.py
+++ nle_code_wrapper/bot/strategies/shop.py
@@ -6,7 +6,7 @@ from nle_utils.glyph import G
 
 from nle_code_wrapper.bot import Bot
 from nle_code_wrapper.bot.inventory import Item, ItemCategory
-from nle_code_wrapper.bot.strategies.goto import goto_glyph
+from nle_code_wrapper.bot.strategies.goto import goto_glyph, goto_room
 from nle_code_wrapper.bot.strategy import strategy
 from nle_code_wrapper.utils.strategies import label_dungeon_features
 
@@ -39,25 +39,20 @@ def leave_shop(bot: "Bot") -> bool:
 
     # while we are in the shop
     while labeled_features[bot.entity.position] == shop_id:
-        distances = bot.pathfinder.distances(bot.entity.position)
-        other_positions = np.argwhere(np.logical_and(labeled_features > 0, labeled_features != shop_id))
-        target_position = min(
-            (tuple(pos) for pos in other_positions if distances.get(tuple(pos)) is not None),
-            key=lambda pos: distances.get(tuple(pos), np.inf),
-            default=None,
-        )
+        # we can exit the shop
+        if goto_room(bot):
+            pass
 
         # the exit is blocked by shopkeeper
-        if target_position is None:
+        else:
             shop_keepers = [entity.position for entity in bot.entities if entity.name == "shopkeeper"]
 
+            distances = bot.pathfinder.compute_distances(bot.entity.position)
             closest_shop_keeper = min(
                 [sk for sk in shop_keepers if bot.pathfinder.reachable(bot.entity.position, sk)],
                 key=lambda sk: distances.get(bot.pathfinder.reachable(bot.entity.position, sk), np.inf),
                 default=None,
             )
-
-            assert closest_shop_keeper is not None
 
             reach = bot.pathfinder.reachable(bot.entity.position, closest_shop_keeper)
 
@@ -70,11 +65,6 @@ def leave_shop(bot: "Bot") -> bool:
                 path = bot.pathfinder.get_path_to(reach)
                 bot.pathfinder.move(path[1])
 
-        # we can exit the shop
-        else:
-            path = bot.pathfinder.get_path_to(target_position)
-            bot.pathfinder.move(path[1])
-
         i += 1
 
         if i > 50:
@@ -82,3 +72,13 @@ def leave_shop(bot: "Bot") -> bool:
             break
 
     return True
+
+
+@strategy
+def buy_items_shop(bot: "Bot") -> bool:
+    pass
+
+
+@strategy
+def sell_items_shop(bot: "Bot") -> bool:
+    pass
