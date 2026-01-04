aaf41a17756b77fce8a0b30fe2b4906202daa329
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2026-01-04 17:54:53 +0100

    update no progress feedback

--- nle_code_wrapper/wrappers/no_progress_feedback.py
+++ nle_code_wrapper/wrappers/no_progress_feedback.py
@@ -8,11 +8,16 @@ class NoProgressFeedback(gym.Wrapper):
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
 
+        return self.populate_obs(obs, info), reward, terminated, truncated, info
+
+    def populate_obs(self, obs, info):
+        obs["text_feedback"] = ""
+
         extra_stats = info.get("episode_extra_stats", {})
         if "env_steps" in extra_stats:
             env_steps = extra_stats["env_steps"]
 
             if env_steps == 0:
-                self.bot.add_message("Your strategy did not result in any progress. Try a different one.")
+                obs["text_feedback"] = "Your strategy did not result in any progress. Try a different one."
 
-        return obs, reward, terminated, truncated, info
+        return obs
